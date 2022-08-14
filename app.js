736a346ece17167b4a0ee8d2bce3f29fff49bd87
pally1995 <51261320+pally1995@users.noreply.github.com>
2022-08-14 08:10:33 +0100

    js complete - investiagte checked not working needed

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 const thankyouCard = document.querySelector(".thankyouCard");
+const feedbackCard = document.querySelector(".feedbackCard");
+const ratingBtns = document.querySelectorAll('input[name="rating"]');
+const submitBtn = document.querySelector(".submitBtn");
+const h3 = document.querySelector("h3");
 
 thankyouCard.style.display = "none";
+
+submitBtn.addEventListener("click", () => {
+  let ratingValue;
+  for (const ratingBtn of ratingBtns) {
+    if (ratingBtn.checked) {
+      ratingValue = ratingBtn.value;
+      h3.innerHTML = `You have selected ${ratingValue} out of 5`
+      thankyouCard.style.display = "block";
+      feedbackCard.style.display = "none";
+      break;
+    } else {
+      feedbackCard.style.display = "block";
+      thankyouCard.style.display = "none";
+    }
+  }
+});
